adef9be37a8d84ab63acbbcde260f4374cf8865a
eliotthbn <151188094+eliotthbn@users.noreply.github.com>
2023-12-26 16:35:11 +0000

    Commit 4

---
+++ ProjetfinalV6/apiRouter.js
@@ -0,0 +1,21 @@
+//Importation des modules
+var express = require('express');
+var usersCtrl = require('./routes/usersCtrl');
+var messagesCtrl = require('./routes/messagesCtrl');
+
+//Configuration des routes
+exports.router = (function(){
+    var apiRouter = express.Router();
+
+    //Routes pour Users
+    apiRouter.route('/users/register/').post(usersCtrl.register);
+    apiRouter.route('/users/login/').post(usersCtrl.login);
+    apiRouter.route('/users/me/').get(usersCtrl.getUserProfile);
+    apiRouter.route('/users/me/').put(usersCtrl.updateUserProfile);
+
+    //Routes pour Messages
+    apiRouter.route('/messages/new/').post(messagesCtrl.createMessage);
+    apiRouter.route('/messages/').get(messagesCtrl.listMessage);
+
+    return apiRouter;
+})();
